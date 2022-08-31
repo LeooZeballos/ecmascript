3b187d9aab5ea015e1c9e3d4b09a78dc4b40d4e5
LeooZeballos <zeballosleonel3@gmail.com>
2022-08-31 08:18:31 -0300

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- es6/index.js
+++ es6/index.js
@@ -16,6 +16,7 @@ function newFunction2(name = 'oscar', age = 23, country = 'MX') {
 newFunction();
 newFunction2();
 
+// --------------------------------------------------------------
 
 // * Concatenacion de Strings con ✨Template Literals✨
 
@@ -29,3 +30,48 @@ console.log(epicPhrase);
 // * es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+// --------------------------------------------------------------
+
+// * Multilinea
+
+// ! before
+let lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.\n' +
+    'Quisquam, quidem. Nam, quisquam.';
+console.log(lorem);
+
+// * es6
+let lorem2 = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Quisquam, quidem. Nam, quisquam.`;
+console.log(lorem2);
+
+// --------------------------------------------------------------
+
+// * Desestructuracion de Objetos
+
+// ! before
+let person = {
+    'name': 'Leonel',
+    'age': 21,
+    'country': 'AR'
+}
+console.log(person.name, person.age, person.country);
+
+// * es6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// --------------------------------------------------------------
+
+// * Operadores de propagacion de errores (spread operator)
+
+// ! before
+let team1 = ['Julian', 'Ramiro', 'Leonel'];
+let team2 = ['Juan', 'Maria', 'Pedro'];
+
+// * es6
+let education = ['David', ...team1, ...team2];
+
+{
+    var globalVar = 'Global Var';
+}
